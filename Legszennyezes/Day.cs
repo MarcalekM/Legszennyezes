d2a0a739d76d40f8c23fb30957978a4dc99c68ad
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-02-16 10:51:53 +0100

    Add project files.

---
+++ Legszennyezes/Day.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Tar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legszennyezes
+{
+    internal class Day
+    {
+        public List<int> _orak;
+
+        public Day(string sor)
+        {
+            var v = sor.Split(';').ToList();
+            _orak = new();
+            foreach (var v2 in v) _orak.Add(int.Parse(v2));
+        }
+
+        public override string ToString()
+        {
+            string text = string.Empty;
+            for (int i = 0; i < _orak.Count; i++)
+            {
+                text += _orak[i].ToString();
+                if (i < _orak.Count) text += ", ";
+            }
+            return text;
+        }
+    }
+}
---
+++ Legszennyezes/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Legszennyezes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Day> days = new();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\SO2.txt",
+                encoding: System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream) days.Add(new(sr.ReadLine()));
+            foreach (Day day in days) Console.WriteLine(day.ToString());
+
+            Console.WriteLine("\n3. feladat:");
+            List<int> f3 = new();
+            int f3Counter = 1;
+            foreach (var day in days)
+            {
+                for (int i = 0; i < day._orak.Count; i++)
+                {
+                    if (day._orak[i] > 250 && !f3.Contains(f3Counter)) f3.Add(f3Counter);
+                }
+                f3Counter++;
+            }
+            foreach (var f in f3) Console.WriteLine($"\tmárcius {f}.");
+
+            Console.WriteLine("\n4. feladat:");
+            int f4 = days[0]._orak[0];
+            int nap = 0;
+            int ora = 0;
+            for (int i = 1; i <= days.Count; i++)
+            {
+                for (int j  = 1; j <= days[i-1]._orak.Count; j++)
+                {
+                    if (days[i - 1]._orak[j-1] > f4)
+                    {
+                        f4 = days[i - 1]._orak[j - 1];
+                        nap = i;
+                        ora = j;
+                    }
+                }
+            }
+            Console.WriteLine($"\t{nap}. nap {ora}. órájában volt a legmagasabb: {f4}");
+
+            Console.WriteLine($"\n5. feladat:");
+            int f5 = 0;
+            for (int i = 1; i <= days.Count; i++)
+            {
+                for (int j = 1; j <= days[i - 1]._orak.Count; j++)
+                {
+                    if (days[i - 1]._orak[j - 1] < 100) f5++;
+                }
+            }
+            Console.WriteLine($"\t{f5} alkalommal esett a koncentrácaió 100 alá");
+
+            Console.WriteLine($"\n6. feladat:");
+            double f6 = 0;
+            int f6Counter = 0;
+            for (int i = 1; i <= days.Count; i++)
+            {
+                for (int j = 1; j <= days[i - 1]._orak.Count; j++)
+                {
+                    f6 += days[i - 1]._orak[j - 1];
+                    f6Counter++;
+                }
+            }
+            f6 = f6 / f6Counter;
+            Console.WriteLine($"\tAz átlag érték a hónapban: {f6}");
+
+            Console.WriteLine($"\n7.feladat:");
+            int f7Counter = 0;
+            nap = 0;
+            for (int i = 1; i <= days.Count; i++)
+            {
+                for (int j = 1; j <= days[i - 1]._orak.Count; j++)
+                {
+                    f7Counter += days[i - 1]._orak[j-1];
+                }
+                if (f7Counter / days[i-1]._orak.Count < 60 && nap == 0)
+                {
+                    nap = i;
+                }
+                f7Counter = 0;
+            }
+            if(nap != 0) Console.WriteLine($"\tA {nap}. napon volt az átlag kevesebb mint 60");
+            else Console.WriteLine("\tNincs ilyen nap");
+
+            Console.WriteLine("\n8. feladat:");
+            var f8 = days[0]._orak;
+            for (int i = 1; i <= f8.Count; i++) if (f8[i-1] == f8.Max()) Console.WriteLine($"\tA {i}. órában volt a legmagasabb az érték");
+
+            Console.WriteLine("\n9. feladat:");
+
+
+            Console.ReadLine();
+        }
+    }
+}
